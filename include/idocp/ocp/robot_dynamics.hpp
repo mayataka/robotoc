d209ea880ac9b0e670b33263e55d9e1f691b1946
mayataka <ihtf4ta7f@gmail.com>
2020-10-05 17:34:07 +0900

    make some functions private

--- include/idocp/ocp/robot_dynamics.hpp
+++ include/idocp/ocp/robot_dynamics.hpp
@@ -47,6 +47,32 @@ public:
                                     const double dtau, const SplitSolution& s, 
                                     KKTResidual& kkt_residual);
 
+  double l1NormRobotDynamicsResidual(const double dtau, 
+                                     const KKTResidual& kkt_residual) const;
+
+  double squaredNormRobotDynamicsResidual(
+      const double dtau, const KKTResidual& kkt_residual) const;
+
+  template <typename MatrixType1, typename MatrixType2, typename MatrixType3, 
+            typename MatrixType4, typename MatrixType5, typename MatrixType6>
+  void getStateFeedbackGain(const Eigen::MatrixBase<MatrixType1>& da_dq,
+                            const Eigen::MatrixBase<MatrixType2>& da_dv,
+                            const Eigen::MatrixBase<MatrixType3>& df_dq,
+                            const Eigen::MatrixBase<MatrixType4>& df_dv,
+                            const Eigen::MatrixBase<MatrixType5>& Kuq,
+                            const Eigen::MatrixBase<MatrixType6>& Kuv) const;
+
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+private:
+  Eigen::VectorXd lu_condensed_, C_floating_base_;
+  Eigen::MatrixXd du_dq_, du_dv_, du_da_, du_df_full_, 
+                  Quu_du_dq_, Quu_du_dv_, Quu_du_da_, Quu_du_df_full_;
+  bool has_floating_base_, has_active_contacts_;
+  int dimf_;
+
+  static constexpr int kDimFloatingBase = 6;
+
   void linearizeInverseDynamics(Robot& robot, 
                                 const ContactStatus& contact_status,
                                 const SplitSolution& s, 
@@ -75,31 +101,6 @@ public:
       const Robot& robot, const ContactStatus& contact_status, 
       const double dtau, KKTResidual& kkt_residual);
 
-  double l1NormRobotDynamicsResidual(const double dtau, 
-                                     const KKTResidual& kkt_residual) const;
-
-  double squaredNormRobotDynamicsResidual(
-      const double dtau, const KKTResidual& kkt_residual) const;
-
-  template <typename MatrixType1, typename MatrixType2, typename MatrixType3, 
-            typename MatrixType4, typename MatrixType5, typename MatrixType6>
-  void getStateFeedbackGain(const Eigen::MatrixBase<MatrixType1>& da_dq,
-                            const Eigen::MatrixBase<MatrixType2>& da_dv,
-                            const Eigen::MatrixBase<MatrixType3>& df_dq,
-                            const Eigen::MatrixBase<MatrixType4>& df_dv,
-                            const Eigen::MatrixBase<MatrixType5>& Kuq,
-                            const Eigen::MatrixBase<MatrixType6>& Kuv) const;
-
-  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-
-private:
-  Eigen::VectorXd lu_condensed_, C_floating_base_;
-  Eigen::MatrixXd du_dq_, du_dv_, du_da_, du_df_full_, 
-                  Quu_du_dq_, Quu_du_dv_, Quu_du_da_, Quu_du_df_full_;
-  bool has_floating_base_, has_active_contacts_;
-  int dimf_;
-
-  static constexpr int kDimFloatingBase = 6;
 
   void setContactStatus(const ContactStatus& contact_status);
 
