a0d90348454c2094b46bc8f8f0b34ffaf7370981
Sotaro Katayama <ihtf4ta7f@gmail.com>
2021-08-25 20:20:03 +0900

    add mpc simulator

---
+++ bindings/python/idocp/mpc/quadrupedal_simulator.py
@@ -0,0 +1,100 @@
+import pybullet
+import pybullet_data
+import numpy as np 
+import math
+import time
+import abc
+
+
+class QuadrupedalSimulator(metaclass=abc.ABCMeta):
+    def __init__(self, path_to_urdf, time_step, start_time, end_time):
+        self.path_to_urdf = path_to_urdf
+        self.time_step = time_step
+        self.start_time = start_time
+        self.end_time = end_time
+        self.calib_camera = False
+        self.camera_distance = 0.0
+        self.camera_yaw = 0.0
+        self.camera_pitch = 0.0
+        self.camera_target_pos = [0., 0., 0.]
+
+    def set_sim_settings(self, time_step, start_time, end_time):
+        self.time_step = time_step
+        self.start_time = start_time
+        self.end_time =end_time
+
+    def set_urdf(self, path_to_urdf):
+        self.path_to_urdf = path_to_urdf
+
+    def set_camera(self, camera_distance, camera_yaw, camera_pitch, camera_target_pos):
+        self.calib_camera = True
+        self.camera_distance = camera_distance
+        self.camera_yaw = camera_yaw
+        self.camera_pitch = camera_pitch
+        self.camera_target_pos = camera_target_pos
+
+    @abc.abstractmethod
+    def get_state_from_pybullet(self, pybullet_robot, q, v):
+        return NotImplementedError()
+
+    @abc.abstractmethod
+    def apply_control_input_to_pybullet(self, pybullet_robot, u):
+        return NotImplementedError()
+
+    @abc.abstractmethod
+    def init_pybullet_robot(self, pybullet_robot, q):
+        return NotImplementedError()
+
+    def print_joint_info(self):
+        pybullet.connect(pybullet.DIRECT)
+        robot = pybullet.loadURDF(self.path_to_urdf, 
+                                  useFixedBase=False, 
+                                  useMaximalCoordinates=False)
+        nJoints = pybullet.getNumJoints(robot)
+        for j in range(nJoints):
+            info = pybullet.getJointInfo(robot, j)
+            print(info)
+        pybullet.disconnect()
+
+    def run_simulation(self, mpc, q0, v0, num_mpc_iteration, record=False, verbose=True):
+        pybullet.connect(pybullet.GUI)
+        pybullet.setGravity(0, 0, -9.81)
+        pybullet.setTimeStep(self.time_step)
+        pybullet.setAdditionalSearchPath(pybullet_data.getDataPath())
+        plane = pybullet.loadURDF("plane.urdf")
+        robot = pybullet.loadURDF(self.path_to_urdf,  
+                                  useFixedBase=False, 
+                                  useMaximalCoordinates=False)
+
+        self.init_pybullet_robot(robot, q0)
+        t = self.start_time
+        q = q0
+        v = v0
+        u = mpc.get_initial_control_input()
+        sim_time = self.end_time - self.start_time
+        sim_steps = math.floor(sim_time/self.time_step)
+
+        if self.calib_camera:
+            pybullet.resetDebugVisualizerCamera(self.camera_distance,
+                                                self.camera_yaw,
+                                                self.camera_pitch,
+                                                self.camera_target_pos)
+
+        if record:
+            pybullet.startStateLogging(pybullet.STATE_LOGGING_VIDEO_MP4, 
+                                       'quadruped_sim.mp4')
+
+        for i in range(sim_steps):
+            self.get_state_from_pybullet(robot, q, v)
+            if verbose:
+                print('t = {:.6g}:'.format(t))
+            mpc.update_solution(t, q, v, num_mpc_iteration, verbose)
+            if verbose:
+                print('KKT error = {:.6g}'.format(mpc.KKT_error(t, q, v)))
+                print('')
+            u = mpc.get_initial_control_input()
+            self.apply_control_input_to_pybullet(robot, u)
+            pybullet.stepSimulation()
+            time.sleep(self.time_step)
+            t = t + self.time_step
+        pybullet.disconnect()
---
+++ examples/anymal_mpc/trotting.py
@@ -0,0 +1,134 @@
+import idocp
+import numpy as np
+from anymal_simulator import ANYmalSimulator
+
+
+LF_foot_id = 12
+LH_foot_id = 22
+RF_foot_id = 32
+RH_foot_id = 42
+contact_frames = [LF_foot_id, LH_foot_id, RF_foot_id, RH_foot_id] 
+path_to_urdf = '../anymal_b_simple_description/urdf/anymal.urdf'
+baumgarte_time_step = 0.05
+robot = idocp.Robot(path_to_urdf, idocp.BaseJointType.FloatingBase, 
+                    contact_frames, baumgarte_time_step)
+
+dt = 0.02
+step_length = 0.15
+step_height = 0.1
+period_swing = 0.5
+t0 = 0.5
+
+cost = idocp.CostFunction()
+q_standing = np.array([0, 0, 0.4842, 0, 0, 0, 1, 
+                       -0.1,  0.7, -1.0, 
+                       -0.1, -0.7,  1.0, 
+                        0.1,  0.7, -1.0, 
+                        0.1, -0.7,  1.0])
+q_weight = np.array([0, 0, 0, 100, 100, 100, 
+                     0.001, 0.001, 0.001, 
+                     0.001, 0.001, 0.001,
+                     0.001, 0.001, 0.001,
+                     0.001, 0.001, 0.001])
+v_weight = np.array([1, 1, 1, 1, 1, 1, 
+                     1, 1, 1, 
+                     1, 1, 1,
+                     1, 1, 1,
+                     1, 1, 1])
+u_weight = np.full(robot.dimu(), 1.0e-02)
+qi_weight = np.array([0, 0, 0, 100, 100, 100, 
+                      1, 1, 1, 
+                      1, 1, 1,
+                      1, 1, 1,
+                      1, 1, 1])
+vi_weight = np.full(robot.dimv(), 1)
+dvi_weight = np.full(robot.dimv(), 0.1)
+# dvi_weight = np.full(robot.dimv(), 0.01)
+config_cost = idocp.ConfigurationSpaceCost(robot)
+config_cost.set_q_ref(q_standing)
+config_cost.set_q_weight(q_weight)
+config_cost.set_qf_weight(q_weight)
+config_cost.set_qi_weight(qi_weight)
+config_cost.set_v_weight(v_weight)
+config_cost.set_vf_weight(v_weight)
+config_cost.set_vi_weight(vi_weight)
+config_cost.set_dvi_weight(dvi_weight)
+config_cost.set_u_weight(u_weight)
+cost.push_back(config_cost)
+
+robot.forward_kinematics(q_standing)
+q0_3d_LF = robot.frame_position(LF_foot_id)
+q0_3d_LH = robot.frame_position(LH_foot_id)
+q0_3d_RF = robot.frame_position(RF_foot_id)
+q0_3d_RH = robot.frame_position(RH_foot_id)
+LF_t0 = t0 + period_swing 
+LH_t0 = t0
+RF_t0 = t0
+RH_t0 = t0 + period_swing 
+LF_foot_ref = idocp.PeriodicFootTrackRef2(q0_3d_LF, step_length, step_height, 
+                                          LF_t0, period_swing, period_swing, False)
+LH_foot_ref = idocp.PeriodicFootTrackRef2(q0_3d_LH, step_length, step_height, 
+                                          LH_t0, period_swing, period_swing, True)
+RF_foot_ref = idocp.PeriodicFootTrackRef2(q0_3d_RF, step_length, step_height, 
+                                          RF_t0, period_swing, period_swing, True)
+RH_foot_ref = idocp.PeriodicFootTrackRef2(q0_3d_RH, step_length, step_height, 
+                                          RH_t0, period_swing, period_swing, False)
+LF_cost = idocp.TimeVaryingTaskSpace3DCost(robot, LF_foot_id, LF_foot_ref)
+LH_cost = idocp.TimeVaryingTaskSpace3DCost(robot, LH_foot_id, LH_foot_ref)
+RF_cost = idocp.TimeVaryingTaskSpace3DCost(robot, RF_foot_id, RF_foot_ref)
+RH_cost = idocp.TimeVaryingTaskSpace3DCost(robot, RH_foot_id, RH_foot_ref)
+foot_track_weight = np.full(3, 1.0e04)
+LF_cost.set_q_weight(foot_track_weight)
+LH_cost.set_q_weight(foot_track_weight)
+RF_cost.set_q_weight(foot_track_weight)
+RH_cost.set_q_weight(foot_track_weight)
+cost.push_back(LF_cost)
+cost.push_back(LH_cost)
+cost.push_back(RF_cost)
+cost.push_back(RH_cost)
+
+com_ref0 = (q0_3d_LF + q0_3d_LH + q0_3d_RF + q0_3d_RH) / 4
+com_ref0[2] = robot.com()[2]
+v_com_ref = np.zeros(3)
+v_com_ref[0] = 0.5 * step_length / period_swing
+com_ref = idocp.PeriodicCoMRef2(com_ref0, v_com_ref, t0, period_swing, 0., True)
+com_cost = idocp.TimeVaryingCoMCost(robot, com_ref)
+com_cost.set_q_weight(np.full(3, 1.0e04))
+cost.push_back(com_cost)
+
+constraints           = idocp.Constraints()
+joint_position_lower  = idocp.JointPositionLowerLimit(robot)
+joint_position_upper  = idocp.JointPositionUpperLimit(robot)
+joint_velocity_lower  = idocp.JointVelocityLowerLimit(robot)
+joint_velocity_upper  = idocp.JointVelocityUpperLimit(robot)
+joint_torques_lower   = idocp.JointTorquesLowerLimit(robot)
+joint_torques_upper   = idocp.JointTorquesUpperLimit(robot)
+constraints.push_back(joint_position_lower)
+constraints.push_back(joint_position_upper)
+constraints.push_back(joint_velocity_lower)
+constraints.push_back(joint_velocity_upper)
+constraints.push_back(joint_torques_lower)
+constraints.push_back(joint_torques_upper)
+constraints.set_barrier(1.0e-01)
+
+T = 0.5
+N = 20
+max_steps = 3
+
+nthreads = 4
+mpc = idocp.MPCQuadrupedalTrotting(robot, cost, constraints, T, N, 
+                                   max_steps, nthreads)
+mpc.set_gait_pattern(step_length, step_height, period_swing, t0)
+q = q_standing
+v = np.zeros(robot.dimv())
+t = 0.0
+mpc.init(t, q, v, 20, verbose=True)
+
+sim_time_step = 0.00333
+sim_start_time = 0.
+sim_end_time = 10.0
+sim = ANYmalSimulator(path_to_urdf, sim_time_step, sim_start_time, sim_end_time)
+
+# sim.set_camera(2.0, 45, -10, q[0:3])
+sim.set_camera(2.0, 45, -10, q[0:3]+np.array([0.5, 0., 0.]))
+sim.run_simulation(mpc, q, v, num_mpc_iteration=5, record=False, verbose=True)
