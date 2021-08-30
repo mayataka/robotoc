234186249da954234e14b939d95f40e129e9deb8
Sotaro Katayama <ihtf4ta7f@gmail.com>
2021-08-30 19:42:17 +0900

    add tests

---
+++ test/cost/periodic_foot_track_ref_test.cpp
@@ -0,0 +1,121 @@
+#include <memory>
+
+#include <gtest/gtest.h>
+#include "Eigen/Core"
+
+#include "idocp/robot/robot.hpp"
+#include "idocp/cost/periodic_foot_track_ref.hpp"
+
+
+namespace idocp {
+
+class PeriodicFootTrackRefTest : public ::testing::Test {
+protected:
+  virtual void SetUp() {
+    srand((unsigned int) time(0));
+    std::random_device rnd;
+    p0 = Eigen::Vector3d::Random();
+
+    t0 = std::abs(Eigen::VectorXd::Random(1)[0]);
+    period_swing = std::abs(Eigen::VectorXd::Random(1)[0]);
+    period_stance = std::abs(Eigen::VectorXd::Random(1)[0]);
+    period = period_swing + period_stance;
+  }
+
+  virtual void TearDown() {
+  }
+
+  Eigen::Vector3d p0;
+  double step_length, step_height, t0, period_swing, period_stance, period;
+};
+
+
+TEST_F(PeriodicFootTrackRefTest, first_mode_half_true) {
+  auto preiodic_foot_ref = std::make_shared<PeriodicFootTrackRef>(p0, step_length,
+                                                                  step_height, t0, 
+                                                                  period_swing,
+                                                                  period_stance, true);
+  Eigen::VectorXd p(3), p_ref(3);
+  const double t1 = t0 - std::abs(Eigen::VectorXd::Random(1)[0]);
+  EXPECT_FALSE(preiodic_foot_ref->isActive(t1));
+  const double t2 = t0 + std::abs(Eigen::VectorXd::Random(1)[0]);
+  preiodic_foot_ref->update_q_3d_ref(t2, p);
+  if (t2 < t0+period_swing) {
+    p_ref = p0;
+    p_ref(0) += 0.5 * ((t2-t0)/period_swing) * step_length;
+    const double tau = t2-t0;
+    const double rate = tau / period_swing;
+    if (rate < 0.5) {
+      p_ref(2) += 2 * step_height * rate;
+    }
+    else {
+      p_ref(2) += 2 * step_height * (1-rate);
+    }
+    EXPECT_TRUE(p.isApprox(p_ref));
+    EXPECT_TRUE(preiodic_foot_ref->isActive(t2));
+  }
+  else if (t2 < period) {
+    EXPECT_FALSE(preiodic_foot_ref->isActive(t2));
+  }
+  else {
+    const int steps = std::floor((t2-t0)/period);
+    const double tau = t2 - t0 - steps*period;
+    if (tau < period_swing) {
+      p_ref = p0;
+      p_ref(0) += (steps-0.5)*step_length; 
+      p_ref(0) += (tau/period_swing)*step_length; 
+      const double rate = tau / period_swing;
+      if (rate < 0.5) {
+        p_ref(2) += 2 * step_height * rate;
+      }
+      else {
+        p_ref(2) += 2 * step_height * (1-rate);
+      }
+      EXPECT_TRUE(p.isApprox(p_ref));
+      EXPECT_TRUE(preiodic_foot_ref->isActive(t2));
+    }
+    else {
+      EXPECT_FALSE(preiodic_foot_ref->isActive(t2));
+    }
+  }
+}
+
+
+TEST_F(PeriodicFootTrackRefTest, first_mode_half_false) {
+  auto preiodic_foot_ref = std::make_shared<PeriodicFootTrackRef>(p0, step_length,
+                                                                  step_height, t0, 
+                                                                  period_swing,
+                                                                  period_stance, false);
+  Eigen::VectorXd p(3), p_ref(3);
+  const double t1 = t0 - std::abs(Eigen::VectorXd::Random(1)[0]);
+  EXPECT_FALSE(preiodic_foot_ref->isActive(t1));
+  const double t2 = t0 + std::abs(Eigen::VectorXd::Random(1)[0]);
+  preiodic_foot_ref->update_q_3d_ref(t2, p);
+  const int steps = std::floor((t2-t0)/period);
+  const double tau = t2 - t0 - steps*period;
+  if (tau < period_swing) {
+    p_ref = p0;
+    p_ref(0) += steps*step_length; 
+    p_ref(0) += (tau/period_swing)*step_length; 
+    const double rate = tau / period_swing;
+    if (rate < 0.5) {
+      p_ref(2) += 2 * step_height * rate;
+    }
+    else {
+      p_ref(2) += 2 * step_height * (1-rate);
+    }
+    EXPECT_TRUE(p.isApprox(p_ref));
+    EXPECT_TRUE(preiodic_foot_ref->isActive(t2));
+  }
+  else {
+    EXPECT_FALSE(preiodic_foot_ref->isActive(t2));
+  }
+}
+
+} // namespace idocp
+
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
