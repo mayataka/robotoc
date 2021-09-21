582616aa7bbe5e88b56d1b06d2b2f512d9754b19
Sotaro Katayama <ihtf4ta7f@gmail.com>
2021-09-21 20:44:24 +0900

    add bindings

--- bindings/python/idocp/__init__.py
+++ bindings/python/idocp/__init__.py
@@ -2,6 +2,7 @@ from .robot import *
 from .ocp import *
 from .cost import *
 from .constraints import *
+from .hybrid import *
 from .solver import *
 from .mpc import *
 from . import utils 
---
+++ bindings/python/idocp/hybrid/__init__.py
@@ -0,0 +1,3 @@
+from .switching_time_cost_function import *
+from .switching_time_cost_function_component_base import *
+from .periodic_switching_time_cost import *
