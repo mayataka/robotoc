d128c16a8dd6e18ee7619a546818d40ad9f76310
mayataka <ihtf4ta7f@gmail.com>
2021-12-31 09:14:09 +0900

    modify example and doc

--- examples/icub/python/jumping_sto.py
+++ examples/icub/python/jumping_sto.py
@@ -9,12 +9,11 @@ contact_frames = [L_foot_id, R_foot_id]
 contact_types = [robotoc.ContactType.SurfaceContact for i in contact_frames]
 path_to_urdf = '../icub_description/urdf/icub.urdf'
 baumgarte_time_step = 0.05
-contact_inv_damping = 1.0e-12
 robot = robotoc.Robot(path_to_urdf, robotoc.BaseJointType.FloatingBase, 
-                      contact_frames, contact_types, baumgarte_time_step, contact_inv_damping)
+                      contact_frames, contact_types, baumgarte_time_step)
 
 dt = 0.02
-jump_length = 0.6
+jump_length = 0.5
 flying_time = 0.25
 ground_time = 0.8
 t0 = 0.
@@ -28,15 +27,15 @@ q_standing = np.array([0, 0, 0.592, 0, 0, 1, 0,
                        0, 0.35, 0.5, 0.5, 0, 0, 0]) # right arm 
 q_ref = q_standing.copy()
 q_ref[0] += jump_length
-q_weight = np.array([0, 0, 0, 100, 100, 100, 
+q_weight = np.array([0, 1, 1, 100, 100, 100, 
                      0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 
                      0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 
                      0.001, 0.001, 0.001,
                      0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 
                      0.001, 0.001, 0.001, 0.001, 0.001, 0.001, 0.001])
 qf_weight = q_weight
-v_weight = np.full(robot.dimv(), 1.0e-02)
-a_weight = np.full(robot.dimv(), 1.0e-04)
+v_weight = np.full(robot.dimv(), 1.0e-03)
+a_weight = np.full(robot.dimv(), 1.0e-05)
 qi_weight = 1.0 * q_weight
 vi_weight = 1.0 * v_weight
 config_cost = robotoc.ConfigurationSpaceCost(robot)
@@ -59,7 +58,8 @@ joint_velocity_upper  = robotoc.JointVelocityUpperLimit(robot)
 joint_torques_lower   = robotoc.JointTorquesLowerLimit(robot)
 joint_torques_upper   = robotoc.JointTorquesUpperLimit(robot)
 mu = 0.6
-wrench_friction_cone  = robotoc.WrenchFrictionCone(robot, mu, 0.1, 0.05)
+# wrench_friction_cone  = robotoc.WrenchFrictionCone(robot, mu, 0.1, 0.05)
+friction_cone  = robotoc.FrictionCone(robot, mu)
 constraints.push_back(joint_position_lower)
 constraints.push_back(joint_position_upper)
 constraints.push_back(joint_velocity_lower)
@@ -67,6 +67,7 @@ constraints.push_back(joint_velocity_upper)
 constraints.push_back(joint_torques_lower)
 constraints.push_back(joint_torques_upper)
 # constraints.push_back(wrench_friction_cone)
+constraints.push_back(friction_cone)
 
 # Create the contact sequence
 max_num_impulses = 3
@@ -108,8 +109,8 @@ ocp = robotoc.OCP(robot=robot, cost=cost, constraints=constraints,
 solver_options = robotoc.SolverOptions()
 solver_options.kkt_tol_mesh = 0.1
 solver_options.max_dt_mesh = T/N 
-solver_options.max_iter = 200
-solver_options.initial_sto_reg_iter = 20 
+solver_options.max_iter = 300
+solver_options.initial_sto_reg_iter = 10 
 ocp_solver = robotoc.OCPSolver(ocp=ocp, contact_sequence=contact_sequence, 
                                solver_options=solver_options, nthreads=4)
 
