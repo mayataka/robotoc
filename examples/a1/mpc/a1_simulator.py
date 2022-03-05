b77e1506e22c1e69a536ad9be5fc57c523c140c7
mayataka <ihtf4ta7f@gmail.com>
2022-03-05 23:57:41 +0900

    add A1 MPC-STO examples

---
+++ examples/a1/mpc/a1_simulator.py
@@ -0,0 +1,105 @@
+import robotoc_sim
+import pybullet
+
+
+class A1Simulator(robotoc_sim.QuadrupedalSimulator):
+    def __init__(self, path_to_urdf, time_step, start_time, end_time):
+        super().__init__(path_to_urdf, time_step, start_time, end_time)
+
+    @classmethod
+    def get_state_from_pybullet(self, pybullet_robot, q, v):
+        # Base
+        basePos, baseOrn = pybullet.getBasePositionAndOrientation(pybullet_robot)
+        q[0:3] = basePos
+        q[3:7] = baseOrn
+        # FL
+        q[7] = pybullet.getJointState(pybullet_robot, 7)[0]
+        q[8] = pybullet.getJointState(pybullet_robot, 9)[0]
+        q[9] = pybullet.getJointState(pybullet_robot, 10)[0]
+        # FR
+        q[10] = pybullet.getJointState(pybullet_robot, 2)[0]
+        q[11] = pybullet.getJointState(pybullet_robot, 4)[0]
+        q[12] = pybullet.getJointState(pybullet_robot, 5)[0]
+        # RF
+        q[13] = pybullet.getJointState(pybullet_robot, 17)[0]
+        q[14] = pybullet.getJointState(pybullet_robot, 19)[0]
+        q[15] = pybullet.getJointState(pybullet_robot, 20)[0]
+        # RH
+        q[16] = pybullet.getJointState(pybullet_robot, 12)[0]
+        q[17] = pybullet.getJointState(pybullet_robot, 14)[0]
+        q[18] = pybullet.getJointState(pybullet_robot, 15)[0]
+
+        # Base
+        baseVel, baseAngVel = pybullet.getBaseVelocity(pybullet_robot)
+        v[0:3] = baseVel
+        v[3:6] = baseAngVel
+        # FL
+        v[6] = pybullet.getJointState(pybullet_robot, 7)[1]
+        v[7] = pybullet.getJointState(pybullet_robot, 9)[1]
+        v[8] = pybullet.getJointState(pybullet_robot, 10)[1]
+        # FR
+        v[9] = pybullet.getJointState(pybullet_robot, 2)[1]
+        v[10] = pybullet.getJointState(pybullet_robot, 4)[1]
+        v[11] = pybullet.getJointState(pybullet_robot, 5)[1]
+        # RF
+        v[12] = pybullet.getJointState(pybullet_robot, 17)[1]
+        v[13] = pybullet.getJointState(pybullet_robot, 19)[1]
+        v[14] = pybullet.getJointState(pybullet_robot, 20)[1]
+        # RH
+        v[15] = pybullet.getJointState(pybullet_robot, 12)[1]
+        v[16] = pybullet.getJointState(pybullet_robot, 14)[1]
+        v[17] = pybullet.getJointState(pybullet_robot, 15)[1]
+
+    @classmethod
+    def apply_control_input_to_pybullet(self, pybullet_robot, u):
+        mode = pybullet.TORQUE_CONTROL
+        # LFL
+        pybullet.setJointMotorControl2(pybullet_robot, 7, controlMode=mode, force=u[0])
+        pybullet.setJointMotorControl2(pybullet_robot, 9, controlMode=mode, force=u[1])
+        pybullet.setJointMotorControl2(pybullet_robot, 10, controlMode=mode, force=u[2])
+        # FR 
+        pybullet.setJointMotorControl2(pybullet_robot, 2, controlMode=mode, force=u[3])
+        pybullet.setJointMotorControl2(pybullet_robot, 4, controlMode=mode, force=u[4])
+        pybullet.setJointMotorControl2(pybullet_robot, 5, controlMode=mode, force=u[5])
+        # RF
+        pybullet.setJointMotorControl2(pybullet_robot, 17, controlMode=mode, force=u[6])
+        pybullet.setJointMotorControl2(pybullet_robot, 19, controlMode=mode, force=u[7])
+        pybullet.setJointMotorControl2(pybullet_robot, 20, controlMode=mode, force=u[8])
+        # RH
+        pybullet.setJointMotorControl2(pybullet_robot, 12, controlMode=mode, force=u[9])
+        pybullet.setJointMotorControl2(pybullet_robot, 14, controlMode=mode, force=u[10])
+        pybullet.setJointMotorControl2(pybullet_robot, 15, controlMode=mode, force=u[11])
+
+
+# pin: FL_hip, FL_thigh, FL_calf, FR_hip, FR_thigh, FR_calf (6, 7, 8, 9, 10, 11)
+# pin: RL_hip, RL_thigh, RL_calf, RR_hip, RR_thigh, RR_calf (12, 13, 14, 15, 16, 17)
+
+    @classmethod
+    def init_pybullet_robot(self, pybullet_robot, q):
+        # Base
+        pybullet.resetBasePositionAndOrientation(pybullet_robot, q[0:3], q[3:7])
+        # FR
+        pybullet.resetJointState(pybullet_robot, 2, q[10])
+        pybullet.resetJointState(pybullet_robot, 4, q[11])
+        pybullet.resetJointState(pybullet_robot, 5, q[12])
+        # FL
+        pybullet.resetJointState(pybullet_robot, 7, q[7])
+        pybullet.resetJointState(pybullet_robot, 9, q[8])
+        pybullet.resetJointState(pybullet_robot, 10, q[9])
+        # RR 
+        pybullet.resetJointState(pybullet_robot, 12, q[16])
+        pybullet.resetJointState(pybullet_robot, 14, q[17])
+        pybullet.resetJointState(pybullet_robot, 15, q[18])
+        # RL 
+        pybullet.resetJointState(pybullet_robot, 17, q[13])
+        pybullet.resetJointState(pybullet_robot, 19, q[14])
+        pybullet.resetJointState(pybullet_robot, 20, q[15])
+        # Turn off the velocity and position control effects
+        joints = [1, 2, 4, 5, 7, 9, 10, 12, 14, 15, 17, 19, 20]
+        for j in joints:
+            pybullet.setJointMotorControl2(pybullet_robot, j, 
+                                           controlMode=pybullet.VELOCITY_CONTROL, 
+                                           force=0.)
+            pybullet.setJointMotorControl2(pybullet_robot, j, 
+                                           controlMode=pybullet.POSITION_CONTROL, 
+                                           force=0.)
---
+++ examples/a1/mpc/jumping.py
@@ -0,0 +1,145 @@
+import robotoc
+import numpy as np
+from a1_simulator import A1Simulator
+
+
+jump_type = 'longitudinal'
+# jump_type = 'lateral'
+# jump_type = 'back'
+# jump_type = 'rotational'
+
+if jump_type == 'longitudinal':
+    jump_length = [0.6, 0, 0]
+    jump_yaw = 0
+elif jump_type == 'lateral':
+    jump_length = [0, 0.4, 0]
+    jump_yaw = 0
+elif jump_type == 'back':
+    jump_length = [-0.3, 0, 0]
+    jump_yaw = 0
+elif jump_type == 'rotational':
+    jump_length = [0.2, 0.0, 0]
+    jump_yaw = np.pi / 6
+
+
+LF_foot_id = 14
+LH_foot_id = 34
+RF_foot_id = 24
+RH_foot_id = 44
+contact_frames = [LF_foot_id, LH_foot_id, RF_foot_id, RH_foot_id] 
+contact_types = [robotoc.ContactType.PointContact for i in contact_frames]
+path_to_urdf = 'a1_description/urdf/a1.urdf'
+baumgarte_time_step = 0.05
+robot = robotoc.Robot(path_to_urdf, robotoc.BaseJointType.FloatingBase, 
+                      contact_frames, contact_types, baumgarte_time_step)
+
+# Create the cost function
+cost = robotoc.CostFunction()
+q_standing = np.array([0, 0, 0.3181, 0, 0, 0, 1, 
+                       0.0,  0.67, -1.3, 
+                       0.0,  0.67, -1.3, 
+                       0.0,  0.67, -1.3, 
+                       0.0,  0.67, -1.3])
+robot.normalize_configuration(q_standing)
+q_ref = q_standing.copy()
+q_ref[0:3] += jump_length
+
+from scipy.spatial.transform import Rotation
+rot_mat = [[np.cos(jump_yaw), -np.sin(jump_yaw), 0],
+           [np.sin(jump_yaw),  np.cos(jump_yaw), 0],
+           [               0,                 0, 1]]
+rot_ref = Rotation.from_matrix(rot_mat).as_quat()
+q_ref[3:7] = rot_ref
+q_weight = np.array([0.0, 100., 100.0, 100., 100., 100., 
+                     0.01, 0.01, 0.01, 
+                     0.01, 0.01, 0.01,
+                     0.01, 0.01, 0.01,
+                     0.01, 0.01, 0.01])
+v_weight = np.full(robot.dimv(), 1.0)
+a_weight = np.full(robot.dimv(), 1.0e-03)
+qi_weight = np.array([0., 1000., 1000., 1000., 1000., 1000., 
+                      10., 10., 10., 
+                      10., 10., 10.,
+                      10., 10., 10.,
+                      10., 10., 10.])
+vi_weight = np.full(robot.dimv(), 10.0)
+dvi_weight = np.full(robot.dimv(), 1.0e-03)
+config_cost = robotoc.ConfigurationSpaceCost(robot)
+config_cost.set_q_ref(q_ref)
+config_cost.set_q_weight(q_weight)
+config_cost.set_qf_weight(qi_weight)
+config_cost.set_qi_weight(qi_weight)
+config_cost.set_v_weight(v_weight)
+config_cost.set_vf_weight(vi_weight)
+config_cost.set_vi_weight(vi_weight)
+config_cost.set_dvi_weight(dvi_weight)
+config_cost.set_a_weight(a_weight)
+cost.push_back(config_cost)
+
+
+constraints           = robotoc.Constraints(barrier=1.0e-03)
+joint_position_lower  = robotoc.JointPositionLowerLimit(robot)
+joint_position_upper  = robotoc.JointPositionUpperLimit(robot)
+joint_velocity_lower  = robotoc.JointVelocityLowerLimit(robot)
+joint_velocity_upper  = robotoc.JointVelocityUpperLimit(robot)
+joint_torques_lower   = robotoc.JointTorquesLowerLimit(robot)
+joint_torques_upper   = robotoc.JointTorquesUpperLimit(robot)
+mu = 0.5
+friction_cone         = robotoc.FrictionCone(robot, mu)
+constraints.push_back(joint_position_lower)
+constraints.push_back(joint_position_upper)
+constraints.push_back(joint_velocity_lower)
+constraints.push_back(joint_velocity_upper)
+constraints.push_back(joint_torques_lower)
+constraints.push_back(joint_torques_upper)
+constraints.push_back(friction_cone)
+
+
+T = 0.8
+N = 18
+
+max_steps = 1
+sto_cost = robotoc.STOCostFunction()
+sto_constraints = robotoc.STOConstraints(max_num_switches=2*max_steps)
+ocp = robotoc.OCP(robot, cost, constraints, sto_cost, sto_constraints, 
+                  T, N, max_steps)
+
+nthreads = 4
+mpc = robotoc.MPCQuadrupedalJumping(ocp, nthreads)
+mpc.set_jump_pattern(jump_length=jump_length, jump_yaw=jump_yaw,
+                     flying_time=0.3, min_flying_time=0.2, 
+                     ground_time=0.3, min_ground_time=0.2)
+
+q = q_standing
+v = np.zeros(robot.dimv())
+t = 0.0
+
+option_init = robotoc.SolverOptions()
+option_init.max_iter = 50 
+option_init.initial_sto_reg_iter = 50
+mpc.init(t, q, v, option_init, sto=True)  
+
+option_mpc = robotoc.SolverOptions()
+option_mpc.max_iter = 2 # MPC iterations
+option_mpc.initial_sto_reg_iter = 0
+option_mpc.max_dt_mesh = T / N
+mpc.set_solver_options(option_mpc)
+
+sim_time_step = 0.0025 # 400 Hz MPC
+sim_start_time = 0.0
+sim_end_time = 1.0
+sim_end_time = 2.0
+sim = A1Simulator(path_to_urdf, sim_time_step, sim_start_time, sim_end_time)
+
+
+if jump_type == 'longitudinal':
+    sim.set_camera(2.0, 35, -0, q[0:3]+np.array([-0.1, 0.5, 0.]))
+elif jump_type == 'lateral':
+    sim.set_camera(2.0, 55, -0, q[0:3]+np.array([-0.1, 0.5, 0.]))
+elif jump_type == 'back':
+    sim.set_camera(1.5, 20, -0, q[0:3]+np.array([-0.4, 0.3, 0.]))
+elif jump_type == 'rotational':
+    sim.set_camera(2.0, 45, -10, q[0:3]+np.array([-0.1, 0.5, 0.]))
+
+sim.run_simulation(mpc, q, v, feedback_delay=True, verbose=True, record=False)
+# sim.run_simulation(mpc, q, v, feedback_delay=True, verbose=True, record=True)
