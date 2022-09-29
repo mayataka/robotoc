c8b7a36ad671770f13ea3023c064e6e79f2d9c54
Sotaro Katayama <ihtf4ta7f@gmail.com>
2022-09-29 17:20:05 +0900

    modify discretization

--- include/robotoc/utils/numerics.hpp
+++ include/robotoc/utils/numerics.hpp
@@ -8,7 +8,7 @@ namespace robotoc {
 namespace numerics {
 
 bool isApprox(const double x, const double y, 
-              const double eps=std::sqrt(std::numeric_limits<double>::epsilon())) ;{
+              const double eps=std::sqrt(std::numeric_limits<double>::epsilon())) {
   return (std::fabs(x-y) < eps);
 }
   
